d8b7becbabec01d746c5b415528af20967a26734
Daniele Lisi <danielelisi978@gmail.com>
2018-02-16 10:08:05 -0800

    :heavy_plus_sign: added requirements.txt

--- smooth_curve.py
+++ smooth_curve.py
@@ -1,7 +1,7 @@
-def smooth_curve(float_num, y_data_arr, x_data_arr):
+def smooth_curve(float_num, x_data_arr, y_data_arr):
     smoothed_y = []
-    shortened_x = x_data_arr[:x_data_arr.size - (float_num)]
+    shortened_x = x_data_arr[:x_data_arr.size - float_num]
     for i in range(y_data_arr.size - float_num):
         current_point = sum(y_data_arr[i:i+float_num])/float_num
         smoothed_y.append(current_point)
-    return [shortened_x , smoothed_y]
+    return [shortened_x, smoothed_y]
