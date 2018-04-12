ffc12996cf5067d9500a04af13d2d10fcdf4dff8
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2018-04-12 14:27:20 -0700

    :art: refactored label output

--- pymodules/ml/predict.py
+++ pymodules/ml/predict.py
@@ -1,25 +1,24 @@
-import keras
-from keras.engine import Input, Model
-from numpy import argmax
-import pandas as pd
-import numpy as np
 import SimpleITK
-from sklearn.preprocessing import LabelEncoder
 from keras.models import load_model
 from util_3d import *
 
-def predict(tumor_img_filepath):
-    labels = ['0-150', '150-300', '300-450', '450-600', '600+']
-    label_encoder = LabelEncoder()
-    y = np.array(labels)
-    label_encoder.fit(y)
-    model = load_model('mri_model.h5')
+def labelPrediction(tumor_img_filepath, labels=['0-150', '150-300', '300-450', '450-600', '600+']):
+    encoded = encodedPrediction(tumor_img_filepath)
+    firstLabel = ("%.2f" % round((encoded[0][0] * 100), 2))
+    secondLabel = ("%.2f" % round((encoded[0][1] * 100), 2))
+    thirdLabel = ("%.2f" % round((encoded[0][2] * 100), 2))
+    fourthLabel = ("%.2f" % round((encoded[0][3] * 100), 2))
+    fifthLabel = ("%.2f" % round((encoded[0][4] * 100), 2))
+    labelPrediction = {labels[0]: firstLabel, labels[1]: secondLabel, labels[2]: thirdLabel, labels[3]: fourthLabel, labels[4]: fifthLabel}
+    print(labelPrediction)
+    return labelPrediction
+
+def encodedPrediction(tumor_img_filepath):
+    model = load_model('mri_modelv2.h5')
     input_image = SimpleITK.ReadImage(tumor_img_filepath)
     rawArray = SimpleITK.GetArrayFromImage(input_image)
     trimmedArray = trim_array_3d(rawArray)
     inputData = trimmedArray.reshape(1, len(trimmedArray), len(trimmedArray[0]), len(trimmedArray[0][0]), 1)
     encodedPrediction = model.predict(inputData)
     print(encodedPrediction)
-    prediction = label_encoder.inverse_transform([argmax(encodedPrediction)])
-    print(prediction)
-    return prediction
+    return encodedPrediction
